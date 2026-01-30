745fe3ea28e14fb2bc3fecf4c13902099ebe1ad9
Hamad <AlmarzooqiH23@gmail.com>
2026-01-30 17:38:45 +0400

    Added new functions, structs and comments.

--- includes/constants.hpp
+++ includes/constants.hpp
@@ -14,10 +14,46 @@
 
 const std::string TITLE("Continues Input Visualizastion");
 //I use C as a prefix to constant
-constexpr uint16_t      CWIDTH = 800;
-constexpr uint16_t      CHEIGHT = 800;
-constexpr unsigned int  WINDOW_OPTIONS = SDL_WINDOW_SHOWN | SDL_WINDOW_MOUSE_GRABBED;
+constexpr int       CWIDTH = 800;
+constexpr int       CHEIGHT = 800;
+constexpr Uint32    WINDOW_OPTIONS = SDL_WINDOW_SHOWN;
+constexpr Uint32    MS_TIMEOUT = 25; //This will be used for the SDL_Delay() function.
 
-// constexpr
+/**
+ * @brief I created this struct the will hold the program data so i wont need to
+ * pass a lot of parameters.
+ * 
+ * @var win Holds a pointer to SDL_Window.
+ * @var window_width Holds the width of the window (Default is CWIDTH)
+ * @var window_height Holds the height of the window (Default is CHeight)
+ * @var frame_factory Holds a pointer to SDL_Renderer which is responsible to draw
+ * and plot stuff on the window.
+ */
+typedef struct GraphicsEngine{
+    SDL_Window      *win;
+    int             window_width;
+    int             window_height;
+    SDL_Renderer    *frameFactory;
 
+} GraphicsEngine;
+
+typedef struct s_rgba{
+    Uint8 r; // Accepted values are 0 - 255.
+    Uint8 g; // Accepted values are 0 - 255.
+    Uint8 b; // Accepted values are 0 - 255.
+    Uint8 a; // Accepted values are 0 - 255.
+} t_rgba;
+
+const t_rgba default_color = {0, 0, 0, 255}; //black
+
+// Event Handlers Functionality
+void    performEvent(bool &runingCond, SDL_Event &sdlEvent);
+
+//Frame Rendering Functionality
+void    clearFrameBuffer(GraphicsEngine &engine);
+void    drawFrame(GraphicsEngine &engine);
+void    updateFrame(GraphicsEngine &engine);
+
+//Clean Up Functionality
+void    cleanup(GraphicsEngine &engine);
 #endif
